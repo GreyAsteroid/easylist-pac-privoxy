fb4bb62e44788508606e05e35bd78ea0f000add6
Eric Karnes <eric@karnes.me>
2020-10-14 00:24:07 -0400

    yes to everything, remove random apple urls?

--- easylist_pac.py
+++ easylist_pac.py
@@ -123,7 +123,8 @@ class EasyListPAC:
         easyprivacy_url = 'https://easylist.to/easylist/easyprivacy.txt'
         easycookie_url = 'https://easylist-downloads.adblockplus.org/easylist-cookie.txt'
         fanboy_annoyance_url = 'https://easylist.to/easylist/fanboy-annoyance.txt'
-        self.download_list = [easycookie_url, fanboy_annoyance_url, easyprivacy_url, easylist_url] + self.extra_easylist_urls
+        antiadblock_url = 'https://easylist-downloads.adblockplus.org/antiadblockfilters.txt'
+        self.download_list = [easycookie_url, antiadblock_url, fanboy_annoyance_url, easyprivacy_url, easylist_url] + self.extra_easylist_urls
         self.file_list = []
         for url in self.download_list:
             fname = os.path.basename(url)
